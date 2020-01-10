2ff004326180e342f93d0c8794c3ca08639a6d08
Ansems <m.p.j.ansems@student.tue.nl>
2020-01-10 10:01:30 +0100

    latest version

---
+++ platforms/android/app/build/generated/source/buildConfig/release/nl/mitchellansems/BrainUp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nl.mitchellansems.BrainUp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "nl.mitchellansems.BrainUp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10011;
+  public static final String VERSION_NAME = "1.1.1";
+}
